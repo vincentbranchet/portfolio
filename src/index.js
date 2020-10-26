d0a65ccc34e3340ce3d5b0c24f16397798c4540b
Vincent Branchet <vince.branchet@gmail.com>
2020-10-26 16:16:59 +0100

    first commit with page structure and basic routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activePage: "projects", // 'projects' || 'info' || 'contact'
+    }
+  }
+
+  routePage() {
+    if(this.state.activePage === "projects") {
+      return <ProjectPage />
+    }
+    else if(this.state.activePage === "info") {
+      return <InfoPage />
+    }
+    else if(this.state.activePage === "contact") {
+      return <ContactPage />
+    }
+    else {
+      // error : wrong page state
+    }
+  }
+
+  handlePageChange(newPage) {
+    if(newPage === "projects") {
+      this.setState({
+        activePage: "projects",
+      });
+    }
+    else if(newPage === "info") {
+      this.setState({
+        activePage: "info",
+      });
+    }
+    else if(newPage === "contact") {
+      this.setState({
+        activePage: "contact",
+      });
+    }
+    else {
+      // error : requested page unknown
+    }
+  }
+
+  render() {
+    const page = this.routePage();
+
+    return (
+      <div className="mainWrapper">
+        <Menu 
+          onClick={(pageName) => this.handlePageChange(pageName)}
+        />
+        <div className="pageWrapper">
+          {page}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <div className="menuWrapper">
+        <button className="menuButton menuInfoButton" onClick={() => this.props.onClick("info")}>Info</button>
+        <button className="menuButton menuContactButton" onClick={() => this.props.onClick("contact")}>Contacts</button>
+        <button className="menuButton menuProjectsButton" onClick={() => this.props.onClick("projects")}>Projects</button>
+      </div>
+    );
+  }
+}
+
+class ProjectPage extends React.Component {
+  render() {
+    return (
+      <div className="projectPageWrapper">Projets</div>
+    );
+  }
+}
+
+class InfoPage extends React.Component {
+  render() {
+    return (
+      <div className="infoPageWrapper">Info</div>
+    );
+  }
+}
+
+class ContactPage extends React.Component {
+  render() {
+    return (
+      <div className="contactPageWrapper">Contact</div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
