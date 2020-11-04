5fe33ab4f5f2a7a0eab28775429d2b41176736ee
Vincent Branchet <vince.branchet@gmail.com>
2020-11-04 14:27:18 +0100

    projects now rendered by lines for a more stable layout

--- src/index.js
+++ src/index.js
@@ -185,7 +185,6 @@ class ProjectPage extends React.Component {
     const projects = this.props.projects.slice(0, this.props.projects.length);
     let selected = [];
     let sorted = [];
-    let rendered = [];
 
     // push all projects that match selected category with their primary category
     for(let i of category) {
@@ -208,12 +207,26 @@ class ProjectPage extends React.Component {
       });
     }
 
-    // sort by score & return project components
+    // sort by score
     sorted = selected.sort((a, b) => b.score - a.score);
 
-    sorted.forEach((proj) => {
-      rendered.push(
-        <Project
+    // returned array has every project matching required categories activated for their primary category first
+    return sorted;
+  }
+
+  renderProjectLineOf(projects, range, startingFrom = 0) {
+    let line = [];
+
+    for(let i = startingFrom; i < range; i++) {
+      if(projects[i]) line.push(this.renderProject(projects[i]));
+    }
+
+    return line;
+  }
+
+  renderProject(proj) { // where proj is an object used to hydrate a Project component
+    return (
+      <Project
         key={proj.id}
         id={proj.id}
         title={proj.title}
@@ -228,19 +241,34 @@ class ProjectPage extends React.Component {
         isSmall={proj.isSmall}
         activeWith={proj.activeWith}
         onClick={(id) => this.props.onClick(id)}
-        />
-      );
-    });
-
-    // returned array has every project matching required categories activated for their primary category first
-    return rendered;
+      />
+    );
   }
 
+  // 1 project per line until 1083px width, 2 projects per line until 1577, 3 projects per line above that
   render() {
     const projects = this.fetchSelected();
+    let rendered = [];
+
+    let xRange = 0;
+    let firstInLine = 0;
+    if(this.props.width < 1083) xRange = 1;
+    else if(this.props.width < 1577) xRange = 2;
+    else xRange = 3;
+    const yRange = Math.ceil(projects.length / xRange);
+    
+    for(let i = 0; i < yRange; i++) {
+      const line = this.renderProjectLineOf(projects, (firstInLine + xRange), firstInLine);
+
+      rendered.push(<div key={i} className={`projectLine`}>{line}</div>);
+
+      firstInLine += xRange;
+    }
 
     return (
-      <div className="projectPageWrapper">{projects}</div>
+      <div className="projectPageWrapper">
+        {rendered}
+      </div>
     );
   }
 }
