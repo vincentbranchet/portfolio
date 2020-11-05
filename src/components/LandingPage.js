cd8b738d4fedd461f7d6fbda381a8a9a369767cb
Vincent Branchet <vince.branchet@gmail.com>
2020-11-05 13:46:37 +0100

    added basic layout w/ reorganized components

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,7 +3,18 @@ import React from 'react';
 class LandingPage extends React.Component {
     render() {
       return (
-        <div className="infoPageWrapper">Info</div>
+        <div className="landingPageWrapper">
+          <div className="landingPresentationWrapper">
+            <Paragraph />
+            <div className="profilePicWrapper">
+              <img src={`${process.env.PUBLIC_URL}/assets/img/profile.png`} />
+            </div>
+          </div>
+          <div className="landingLinksWrapper">
+            <Link />
+            <Link />
+          </div>
+        </div>
       );
     }
 }
