1b61c7aa41b0d02b239547c0c6bd4fd866bb66c7
Vincent Branchet <vince.branchet@gmail.com>
2020-11-20 13:18:04 +0100

    upgraded the design of the landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,10 +7,24 @@ class LandingPage extends React.Component {
       return (
         <div className={`landingPageWrapper ${this.props.direction}`}>
           <div className="landingPresentationWrapper">
-            <Paragraph
-              text={this.props.text}
-            />
+            <div className="webPresWrapper">
+              <img className="webPresLogo" src={`${process.env.PUBLIC_URL}/assets/img/coding.png`} alt="" />
+              <div>Développement web front-end</div>
+            </div>
+            <div className="gamesPresWrapper">
+              <img className="gamesPresLogo" src={`${process.env.PUBLIC_URL}/assets/img/gamepad.png`} alt="" />
+              <div>Narrative & game design</div>
+            </div>
+            <div className="litPresWrapper">
+              <img className="litPresLogo" src={`${process.env.PUBLIC_URL}/assets/img/quill.png`} alt="" />
+              <div>Écriture créative</div>
+            </div>
+            <div className="availablePresWrapper">
+              <img className="availablePresLogo" src={`${process.env.PUBLIC_URL}/assets/img/ok-mark.png`} alt="" />
+              <div>Disponible !</div>
+            </div>
           </div>
+          
           <div className="profilePicWrapper">
             <img src={`${process.env.PUBLIC_URL}/assets/img/profile.png`} />
           </div>
